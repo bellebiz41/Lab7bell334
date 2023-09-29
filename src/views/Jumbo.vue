c33beee3af4328ef846943d443d1d6f52f00150e
bellebiz41 <bellebiz41@gmail.com>
2023-09-29 16:13:56 +0700

    first commit

---
+++ src/views/Jumbo.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  text_title: {
+    type: String,
+    required: true
+  },
+  ex_box: {
+    type: String,
+    required: true
+  }
+})
+</script>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,7 @@
+<script setup>
+import newcard from '../components/NewsCard.vue'
+</script>
+
+<template>
+  <newcard></newcard>
+</template>
